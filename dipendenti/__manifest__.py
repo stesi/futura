989ddd1b3741096c74a4d04f8ea6b652c2888f84
Luca-Cocozza <luca.cocozza@futurasl.com>
2023-05-24 11:34:41 +0200

    Add state_vehicle and euro in fleet.vehicle - fix 8

--- dipendenti/__manifest__.py
+++ dipendenti/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '0.2',
     'author': "Luca Cocozza",
     'application': True,
-    'depends': ['hr', 'fleet'],
+    'depends': ['hr', 'fleet', 'fleet_replacement', 'fleet_service_with_deduction',],
     'data': [
         # # Settaggi per accesso ai contenuti
         'data/ir.model.access.csv',
