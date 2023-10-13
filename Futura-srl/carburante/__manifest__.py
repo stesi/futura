5ddd8e9832484ec4a89c960bc5e63c88be7f677e
Luca-Cocozza <luca.cocozza@futurasl.com>
2023-10-13 17:09:29 +0200

    Aggiunto modulo carburante

---
+++ Futura-srl/carburante/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'carburante',
+    'version': '16',
+    'author': "Luca Cocozza",
+    'application': True,
+    'description': "Gestione dei rifornimenti relativi alla flotta aziendale.",
+    'depends': ['hr', 'fleet', 'gtms_fleet_organization', 'fleet_limited_traffic_zone', 'gtms_fleet_service_with_deduction', 'fleet_replacement',],
+    'data': [
+        # # Settaggi per accesso ai contenuti
+        'data/ir.model.access.csv',
+        # # Caricamento delle view,
+        'view/fleet_fuel_view.xml',
+        'view/fleet_fuel_type_view.xml',
+        'view/fleet_fuel_company_view.xml',
+        # Menu
+        'view/menu.xml',
+    ],
+}
---
+++ Futura-srl/carburante/models/fleet_fuel.py
@@ -0,0 +1,61 @@
+from odoo import fields, models
+
+
+class FleetFuelType(models.Model):
+    _name = "fleet.fuel.type"
+    _description = "Tipi di carburante"
+    
+    name = fields.Char()
+    
+    
+class FleetFuelCompany(models.Model):
+    _name = "fleet.fuel.company"
+    _description = "Tabella per associare azienda interna con codice azienda carburante"
+
+    name = fields.Char(compute="_compute_name_company")
+    company_reference = fields.Char(string="Company Reference")
+    company_id = fields.Many2one('res.company')
+    
+    def _compute_name_company(self):
+        for record in self:
+            if record.company_id:
+                record.name = record.company_id.name
+            else:
+                record.name = ''
+
+
+class FleetFuel(models.Model):
+    _name = "fleet.fuel"
+    _description = "Tabella rifornimenti mezzi"
+
+    name = fields.Char(compute='_compute_res_partner_id')
+    res_partner_id = fields.Many2one('res.partner', string='Partner', domain="[('is_company','=',True)]")
+    reference = fields.Char(string='Reference')
+    point_sale_code = fields.Char(string='Point Sale Code')
+    location = fields.Char(string='Location')
+    address = fields.Char(string='Address')
+    transaction_datetime = fields.Datetime(string='Transaction Date and Time')
+    ticket = fields.Char(string='Ticket')
+    km_fleet = fields.Char(string="Km at moment")
+    driver_id = fields.Many2one('res.partner', string='Driver', domain="[('is_driver','=',True)]")
+    card_number = fields.Char(string='Card Number')
+    fleet_id = fields.Many2one('fleet.vehicle', string='Vehicle')
+    product_id = fields.Many2one('fleet.fuel.type', string='Fuel Type')
+    quantity = fields.Float(string='Quantity')
+    price_unit = fields.Float(string='Price per Unit', digits=(3, 3))
+    price = fields.Float(string='Total Price', digits=(3, 3))
+    tax = fields.Float(string='Tax', digits=(3, 3))
+    business_name_id = fields.Many2one('res.company', string='company reference')
+    invoice = fields.Char(string='Invoice')
+    invoice_date = fields.Date(string='Invoice Date')
+    invoice_discount = fields.Float(string='Invoice Discount', digits=(3, 3))
+    tax_rate = fields.Float(string='Tax Rate', digits=(3, 3))
+    price_whitout_tax = fields.Float(string='Price Without Tax', digits=(6, 3))
+    
+    def _compute_res_partner_id(self):
+        for record in self:
+            if record.reference:
+                record.name = record.reference
+            else:
+                record.name = ''
+
