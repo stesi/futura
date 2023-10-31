d852b8376119c266d02b776657594f52ef81c6b1
Luca-Cocozza <luca.cocozza@futurasl.com>
2023-10-31 12:06:35 +0100

    test7

--- Futura-srl/diritti3/models/user_groups.py
+++ Futura-srl/diritti3/models/user_groups.py
@@ -5,7 +5,7 @@ class Diritti(models.Model):
     _inherit = 'fleet.vehicle.log.services'
 
 
-    groups_ids = fields.Char(string='Groups of the User', compute='_compute_user_groups')
+    groups_ids = fields.Many2many('res.groups', string='Groups of the User', compute='_compute_user_groups')
     is_rop = fields.Boolean(compute='_compute_is_rop')
 
     @api.depends()
@@ -15,8 +15,6 @@ class Diritti(models.Model):
             user = self.env.user
             # Ottieni gli identificatori dei gruppi dell'utente connesso
             record.groups_ids = user.groups_id.ids
-            if 116 in record.groups_ids:
-                record.is_rop = True
 
     @api.depends('groups_ids')
     def _compute_is_rop(self):
