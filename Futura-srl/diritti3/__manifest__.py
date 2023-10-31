f1afcfb0cd55c0c7c929414fd10f6b86627aaa74
Luca-Cocozza <luca.cocozza@futurasl.com>
2023-10-31 09:33:22 +0100

    test

---
+++ Futura-srl/diritti3/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'diritti',
+    'version': '16',
+    'author': "Luca Cocozza",
+    'application': True,
+    'description': "Aggiunta del campo groups_ids da poter integrare nei moduli.",
+    'depends': [],
+    'data': [
+        # # Settaggi per accesso ai contenuti
+        #'data/ir.model.access.csv',
+        # # Caricamento delle view,
+        'view/groups_field.xml',
+    ],
+}
---
+++ Futura-srl/diritti3/models/user_groups.py
@@ -0,0 +1,19 @@
+from odoo import fields, models
+
+
+class FleetFuelType(models.Model):
+    _inherit = ['fleet.vehicle.log.services']
+
+
+    groups_ids = fields.Char(string='Groups of the User', compute='_compute_user_groups')
+    is_rop = fields.Boolean(compute='_compute_is_rop')
+
+    @api.depends()
+    def _compute_user_groups(self):
+        for record in self:
+            # Trova l'utente connesso
+            user = self.env.user
+            # Ottieni gli identificatori dei gruppi dell'utente connesso
+            record.groups_ids = user.groups_id.ids
+            if 116 in record.groups_ids:
+                record.is_rop = True
